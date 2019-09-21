68629bbc1e67e1541544b2041a50f1949d465162
Jesse Norris <emrys90@gmail.com>
2019-09-21 16:52:09 -0500

    Add in angular jwt for handling authentication token

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
     model: any = {};
 
-    constructor(private authService: AuthService, private alertify: AlertifyService) {}
+    constructor(public authService: AuthService, private alertify: AlertifyService) {}
 
     ngOnInit() {}
 
@@ -26,12 +26,11 @@ export class NavComponent implements OnInit {
     }
 
     loggedIn() {
-        const token = localStorage.getItem('token');
-        return !!token;
+        return this.authService.loggedIn();
     }
 
     logout() {
-        localStorage.removeItem('token');
+        this.authService.logout();
         this.alertify.message('logged out');
     }
 }
